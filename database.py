89547afc8530bed400d4618891a1368c28d20c94
moz123m <moz123m@gmail.com>
2025-07-18 11:20:19 +0300

    Edit session generator at database file

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
+from typing import AsyncGenerator
 
 DATABASE_URL = "sqlite+aiosqlite:///./emailassistant.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
@@ -14,6 +14,6 @@ async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]: # Database session generator
     async with async_session() as session:
         yield session
